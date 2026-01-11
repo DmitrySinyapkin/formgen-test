a743b4bc4ef537b1e54b519fb9f01207a9e91dee
DmitrySinyapkin <dsinyapkin@mail.ru>
2026-01-11 18:01:31 +0300

    [initial values] - fixed initial values getter

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -1,7 +1,9 @@
 import type { Form, FormFields } from "@/types/form"
 
-const getEmptyValues = (fields: FormFields) => 
-    Object.fromEntries(Object.entries(fields).map(([name, field]) => field.type === 'checkbox' ? [name, false] : [name, '']))
-
 export const getInitialValues = (form: Form) => 
-    form.initialValues || getEmptyValues(form.fields)
+    Object.fromEntries(Object.entries(form.fields).map(([name, field]) => form.initialValues?.[name]
+        ? [name, form.initialValues[name]] 
+        : field.type === 'checkbox'
+        ? [name, false] 
+        : [name, '']
+    ))
